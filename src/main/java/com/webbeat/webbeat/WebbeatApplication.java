96723a13092d6bf670a0c64cebab14e8a76a7836
Mateus Torres <mmtorres230726@gmail.com>
2025-10-21 16:05:40 -0300

    Iniacializando o projeto

---
+++ src/main/java/com/webbeat/webbeat/WebbeatApplication.java
@@ -0,0 +1,13 @@
+package com.webbeat.webbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webbeat/webbeat/WebbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbeat.webbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
