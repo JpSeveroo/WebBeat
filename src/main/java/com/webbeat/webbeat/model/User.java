c490faa89d6dc53df26b76353b8539ea1a74f952
João Pedro Severo <142758081+JpSeveroo@users.noreply.github.com>
2025-11-20 20:13:31 -0300

    .

--- src/main/java/com/webbeat/webbeat/model/User.java
+++ src/main/java/com/webbeat/webbeat/model/User.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
-public record User(
+public record User( //Só pra explicar, tamo usando record por tratarmos de dados imutáveis
         @Id
         String id,
         String email,
