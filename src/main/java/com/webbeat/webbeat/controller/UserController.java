793945fc1b51a5a4afe9d305f54aed3ccead0ab0
Mateus Torres <mmtorres230726@gmail.com>
2025-12-03 17:02:45 -0300

    apagando alterações desnecessarias no UserController

--- src/main/java/com/webbeat/webbeat/controller/UserController.java
+++ src/main/java/com/webbeat/webbeat/controller/UserController.java
@@ -1,17 +1,11 @@
 package com.webbeat.webbeat.controller;
 
-import com.webbeat.webbeat.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-
-    UserService userService;
-
     @GetMapping("/settings")
     public String settingsPage(Model model) {
         model.addAttribute("activePage", "settings");
