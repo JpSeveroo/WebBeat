7e50db2327492ce6440efa7ee83f0f38ca3d7039
Mateus Torres <mmtorres230726@gmail.com>
2025-12-03 15:55:25 -0300

    adicionando a dependência, a configuração de ambiente o model e o repository para criar o serviço de esqueci senha da aplicação

---
+++ src/main/java/com/webbeat/webbeat/repository/PasswordResetRepository.java
@@ -0,0 +1,11 @@
+package com.webbeat.webbeat.repository;
+
+import com.webbeat.webbeat.model.PasswordResetToken;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface PasswordResetRepository extends MongoRepository<PasswordResetToken, String> {
+
+    PasswordResetToken findByToken(String token);
+
+}
